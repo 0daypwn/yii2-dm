d969a49ca0c9094242552f49c17555413b1c203a
wuxb02 <wuxb02@mingyuanyun.com>
2022-12-21 16:42:04 +0800

    适配yii2低版本

--- src/Schema.php
+++ src/Schema.php
@@ -14,8 +14,6 @@ use yii\db\CheckConstraint;
 use yii\db\ColumnSchema;
 // use yii\db\Connection;
 use yii\db\Constraint;
-use yii\db\ConstraintFinderInterface;
-use yii\db\ConstraintFinderTrait;
 use yii\db\Expression;
 use yii\db\ForeignKeyConstraint;
 use yii\db\IndexConstraint;
@@ -31,9 +29,31 @@ use yii\helpers\ArrayHelper;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class Schema extends \yii\db\Schema implements ConstraintFinderInterface
+class Schema extends \yii\db\Schema
 {
-    use ConstraintFinderTrait;
+    // The following are the supported abstract column data types.
+    const TYPE_PK = 'pk';
+    const TYPE_UPK = 'upk';
+    const TYPE_BIGPK = 'bigpk';
+    const TYPE_UBIGPK = 'ubigpk';
+    const TYPE_CHAR = 'char';
+    const TYPE_STRING = 'string';
+    const TYPE_TEXT = 'text';
+    const TYPE_TINYINT = 'tinyint';
+    const TYPE_SMALLINT = 'smallint';
+    const TYPE_INTEGER = 'integer';
+    const TYPE_BIGINT = 'bigint';
+    const TYPE_FLOAT = 'float';
+    const TYPE_DOUBLE = 'double';
+    const TYPE_DECIMAL = 'decimal';
+    const TYPE_DATETIME = 'datetime';
+    const TYPE_TIMESTAMP = 'timestamp';
+    const TYPE_TIME = 'time';
+    const TYPE_DATE = 'date';
+    const TYPE_BINARY = 'binary';
+    const TYPE_BOOLEAN = 'boolean';
+    const TYPE_MONEY = 'money';
+    const TYPE_JSON = 'json';
 
     /**
      * @var array map of DB errors and corresponding exceptions
